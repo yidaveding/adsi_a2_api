108de8777e86cc039e60e8a4bc18feb37726227c
yidaveding <yi.dave.ding@gmail.com>
2021-03-12 11:42:44 +1100

    heroku

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ COPY requirements.txt .
 
 RUN pip3 install -r requirements.txt
 
+RUN pip install torch==1.7.1+cpu torchvision==0.8.2+cpu torchtext==0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
+
 COPY ./app /app
 
 COPY ./models /models
--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from starlette.responses import JSONResponse
-from joblib import load
+import joblib
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 import pandas as pd
 
 '''
@@ -13,11 +16,28 @@ import pandas as pd
 
 app = FastAPI()
 
-rf_pipe = load('../models/random_forest_base.joblib')
+rf_pipe = joblib.load('../models/random_forest_base.joblib')
+sc = joblib.load('../models/standard_scaler.joblib')
+enc = joblib.load('../models/output_encoder.joblib')
 
-def format_features(brewery_name:str, review_aroma:int, review_appearance:int, review_palate:int, review_taste:int):
+class PytorchMultiClass(nn.Module):
+    def __init__(self, num_features):
+        super(PytorchMultiClass, self).__init__()
+
+        self.layer_1 = nn.Linear(num_features, 128)
+        self.layer_out = nn.Linear(128, 104)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        x = F.dropout(F.relu(self.layer_1(x)), training=self.training)
+        x = self.layer_out(x)
+        return self.softmax(x)
+
+nn_model = PytorchMultiClass(4)
+nn_model.load_state_dict(torch.load('../models/pt_base_state_dict'))
+
+def format_features(review_aroma:int, review_appearance:int, review_palate:int, review_taste:int):
   return {
-        'brewery_name': [brewery_name],
         'review_aroma': [review_aroma],
         'review_appearance': [review_appearance],
         'review_palate': [review_palate],
@@ -30,20 +50,25 @@ def read_root():
 
 @app.get("/health", status_code=200)
 def healthcheck():
-    return 'Welcome to predictor API, the endpoint is ready'
+    return 'Welcome to predictor API, \n the endpoint is ready'
 
-@app.post("/beer/type")
+@app.post("/rf/beer/type")
 def predict_beer(brewery_name:str, review_aroma:int, review_appearance:int, review_palate:int, review_taste:int):
-    features = format_features(brewery_name, review_aroma, review_appearance, review_palate, review_taste)
-    obs = pd.DataFrame(features)
-    pred = rf_pipe.predict(obs)
+    features = format_features(review_aroma, review_appearance, review_palate, review_taste)
+    obs = sc.transform(pd.DataFrame(features))
+    obst = torch.from_numpy(obs).float()
+    pred = nn_model(obst)
+    pred = pred.argmax(1)
+    pred = enc.inverse_transform(pred)
     return JSONResponse(pred.tolist())
 
-@app.post("/beers/type")
+@app.post("/rf/beers/type")
 def predict_beers(df):
-    pred = rf_pipe.predict(df)
+    pred = enc.inverse_transform(nn_model(df))
     return JSONResponse(pred.tolist())
 
-# @app.get("/model/architecture/")
-# def get_architecture():
-#     return rf_pipe
+@app.get("/model/architecture/")
+def get_architecture():
+    mod = nn_model._modules
+    arch = {i:mod[i] for i in mod}
+    return str(arch)
