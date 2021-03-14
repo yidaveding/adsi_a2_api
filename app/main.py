f068d65f7c5b49be880ed97a8b8a2ae7a4c1fd99
yidaveding <yi.dave.ding@gmail.com>
2021-03-15 01:39:37 +1100

    api final

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import pandas as pd
+import json
 
 '''
 ‘/’ (GET): Displaying a brief description of the project objectives, list of endpoints, expected input parameters and output format of the model, link to the Github repo related to this project
@@ -21,16 +22,25 @@ sc = joblib.load('../models/standard_scaler.joblib')
 enc = joblib.load('../models/output_encoder.joblib')
 
 class PytorchMultiClass(nn.Module):
-    def __init__(self, num_features):
+    def __init__(self, num_features=4, hlayer_neurons=256):
         super(PytorchMultiClass, self).__init__()
 
-        self.layer_1 = nn.Linear(num_features, 128)
-        self.layer_out = nn.Linear(128, 104)
+        self.layer_1 = nn.Linear(num_features, hlayer_neurons)
+        self.layer_2 = nn.Linear(hlayer_neurons, hlayer_neurons)
+        self.layer_3 = nn.Linear(hlayer_neurons, hlayer_neurons)
+        self.layer_out = nn.Linear(hlayer_neurons, 104)
         self.softmax = nn.Softmax(dim=1)
 
+        self.dropout = nn.Dropout(0.5)
+
     def forward(self, x):
-        x = F.dropout(F.relu(self.layer_1(x)), training=self.training)
-        x = self.layer_out(x)
+        x = F.relu(self.layer_1(x))
+        x = self.dropout(x)
+        x = F.relu(self.layer_2(x))
+        x = self.dropout(x)
+        x = F.relu(self.layer_3(x))
+        x = self.dropout(x)
+        x = F.relu(self.layer_out(x))
         return self.softmax(x)
 
 nn_model = PytorchMultiClass(4)
@@ -53,7 +63,7 @@ def healthcheck():
     return 'Welcome to predictor API, \n the endpoint is ready'
 
 @app.post("/rf/beer/type")
-def predict_beer(brewery_name:str, review_aroma:int, review_appearance:int, review_palate:int, review_taste:int):
+def predict_beer(brewery_name:str, review_aroma:float, review_appearance:float, review_palate:float, review_taste:float):
     features = format_features(review_aroma, review_appearance, review_palate, review_taste)
     obs = sc.transform(pd.DataFrame(features))
     obst = torch.from_numpy(obs).float()
@@ -63,9 +73,26 @@ def predict_beer(brewery_name:str, review_aroma:int, review_appearance:int, revi
     return JSONResponse(pred.tolist())
 
 @app.post("/rf/beers/type")
-def predict_beers(df):
-    pred = enc.inverse_transform(nn_model(df))
-    return JSONResponse(pred.tolist())
+def predict_beers(beer_dict):
+    y_pred_list = []
+    beer_dict = json.loads(beer_dict)
+    for i in beer_dict:
+        aroma = beer_dict[i]['review_aroma']
+        appearance = beer_dict[i]['review_appearance']
+        palate = beer_dict[i]['review_palate']
+        taste = beer_dict[i]['review_taste']
+
+        features = format_features(aroma, appearance, palate, taste)
+        obs = sc.transform(pd.DataFrame(features))
+        obst = torch.from_numpy(obs).float()
+        pred = nn_model(obst)
+        pred = pred.argmax(1)
+        pred = enc.inverse_transform(pred)
+
+        y_pred_list.append(pred.tolist())
+
+        print(y_pred_list)
+    return JSONResponse(y_pred_list)
 
 @app.get("/model/architecture/")
 def get_architecture():
